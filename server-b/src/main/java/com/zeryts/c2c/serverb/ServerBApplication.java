890f0f2c30f7df70ff161a59c8e31e729aae4671
hezitao1122 <330984177@qq.com>
2020-11-08 23:43:02 +0800

    初始化

---
+++ server-b/src/main/java/com/zeryts/c2c/serverb/ServerBApplication.java
@@ -0,0 +1,13 @@
+package com.zeryts.c2c.serverb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerBApplication.class, args);
+    }
+
+}
---
+++ server-b/src/main/java/com/zeryts/c2c/serverb/TestController.java
@@ -0,0 +1,15 @@
+package com.zeryts.c2c.serverb;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zeryts
+ * @description: 测试调用的controller类
+ * ```````````````````````````
+ * @title: TestController
+ * @projectName c2c-shop
+ * @date 2020/11/8 21:38
+ */
+@RestController
+public class TestController {
+}
