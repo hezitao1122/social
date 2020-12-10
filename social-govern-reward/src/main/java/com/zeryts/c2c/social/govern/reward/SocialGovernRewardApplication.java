3d8c961085f3cd0a5f1b3a7dff95b8c8a756aae8
hezitao1122 <330984177@qq.com>
2020-12-10 15:45:23 +0800

    升级cloud和boot 加入注册中心

--- social-govern-reward/src/main/java/com/zeryts/c2c/social/govern/reward/SocialGovernRewardApplication.java
+++ social-govern-reward/src/main/java/com/zeryts/c2c/social/govern/reward/SocialGovernRewardApplication.java
@@ -4,7 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+/**
+ * @description: 启动类
+ * @title: RewardServiceImpl
+ * @projectName: c2c-social-govern
+ * @author: zeryts
+ * @date: 2020/11/29-7:15
+ */
 @SpringBootApplication
 @RestController
 public class SocialGovernRewardApplication {
