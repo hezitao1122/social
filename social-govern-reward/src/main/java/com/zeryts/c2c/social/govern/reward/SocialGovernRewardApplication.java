6907a4f746dc9b659f9ab7d315982bd7daa48aee
hezitao1122 <330984177@qq.com>
2020-12-10 08:26:51 +0800

    sentinel客户端使用

--- social-govern-reward/src/main/java/com/zeryts/c2c/social/govern/reward/SocialGovernRewardApplication.java
+++ social-govern-reward/src/main/java/com/zeryts/c2c/social/govern/reward/SocialGovernRewardApplication.java
@@ -2,12 +2,18 @@ package com.zeryts.c2c.social.govern.reward;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SocialGovernRewardApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SocialGovernRewardApplication.class, args);
     }
-
+    @GetMapping("/say")
+    public String say() {
+        return "say";
+    }
 }
